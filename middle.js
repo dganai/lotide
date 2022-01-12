e9aebb6a080e7c40a4f1a6f5ded1f0261a2752ac
dganai <dinaganai@gmail.com>
2022-01-12 20:25:18 +0000

    creating middle of array function

---
+++ middle.js
@@ -0,0 +1,55 @@
+// copy assertArraysEqual and eqArrays function
+
+const eqArrays = function(array1, array2) {
+  for (let i = 0; i < array1.length; i++) {
+    for (let j = 0; j < array2.length; j++) {
+      if (array1[i] === array2[j]) {
+        return true;
+      } else {
+        return   false;
+      }
+    }
+  }
+};
+
+const assertArraysEqual = function(array1, array2) {
+  eqArrays(array1, array2) ? console.log(true) : console.log(false);
+};
+
+// create middle
+// take in an array and return middle most elements of given array
+// exceptions: if elements 1 or 2, return empty array
+// odd number of elements: return single middle element
+// even number of elements: return two elements in middle
+
+const middle =  function(array) {
+  let newArray = [];
+  let theMiddle = array[Math.floor(array.length / 2)];
+  let numOfElements = array.length;
+  
+  if (numOfElements <= 2) {
+    newArray = [];
+  }
+  if (numOfElements > 2) {
+    if (numOfElements % 2 === 0) { // even number of elements
+      newArray = [array[theMiddle] - 2, array[theMiddle] - 1];
+    } else if (numOfElements % 2 === 1) {// odd number elements
+      newArray = [theMiddle];
+    
+    }
+  }
+  // console.log(newArray);
+  return newArray;
+};
+
+// test below
+
+console.log(middle([1]));
+console.log(middle([1, 2]));
+console.log(middle([1, 2, 3, 4, 5]));
+console.log(middle([1, 2, 3, 4, 5, 6]));
+console.log(middle([1, 2, 3, 4, 5, 6, 7, 8, 9]));
+
+assertArraysEqual(middle([1, 2, 3]), [2]);
+assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
+assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
