7dfca49959d69ec4118406acb35b8a53fd29172f
dganai <dinaganai@gmail.com>
2022-01-13 18:40:54 +0000

    changes to test

--- eqObjects.js
+++ eqObjects.js
@@ -42,12 +42,9 @@ const eqObjects = (object1, object2) => {
 const cd = { c: "1", d: ["2", 3] };
 const cd2 = { c: "1", d: ["2", 3, 4] };
 const ab = { a: "1", b: "2" };
-const ba = { b: "2", a: "1" };
-const abc = { a: "1", b: "2", c: "3" };
-const dc = { d: ["2", 3], c: "1" };
 
+// use assertEqual to show result
 
 assertEqual(eqObjects(cd, cd2), false);
 assertEqual(eqObjects(ab, ba), true);
-assertEqual(eqObjects(ab, abc), false);
-assertEqual(eqObjects(cd, dc), true);
+
