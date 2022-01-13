6437396c3780a316784da6d746ef5b56b3322bca
dganai <dinaganai@gmail.com>
2022-01-13 15:00:30 +0000

    no indices in v1, refactored to include indices where each character is found

--- letterPositions.js
+++ letterPositions.js
@@ -17,18 +17,31 @@ const assertArraysEqual = function(array1, array2) {
 const letterPositions = function(sentence) {
   const results = {};
   // logic to update results here
-  for (let i = 0; i < sentence.length; i++) {
-    if (results[sentence[i]]) {
-      results[sentence[i]].push(i)
-    } else if (sentence[i] === " ") {
-      
-    } else {
-      results[sentence[i]] = [i];
+  // for in loop
+  let string = sentence;
+  for (let index in string) {
+    let letter = string[index];
+    if (letter !== " ") {
+      if (results[letter]) {
+        results[letter].push(+index);
+      } else {
+        results[letter] = [+index];
+      }
     }
   }
+  console.log(results);
+
   return results;
 };
 
+
+// test below
+
+letterPositions("lighthouse in the house");
+
+
+
+letterPositions("lighthouse in the house");
 assertArraysEqual(letterPositions("hello").e, [1]);
 assertArraysEqual(letterPositions("hello").o, [3]);
 assertArraysEqual(letterPositions("hello").h, [0]);
