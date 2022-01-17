0f4553313540fcf30b834dc9f643a12818b71fed
dganai <dinaganai@gmail.com>
2022-01-17 18:42:04 +0000

    refactor

--- tail.js
+++ tail.js
@@ -1,7 +1,11 @@
 //tail function returns new array
 
 const tail = function(arr) {
-  return arr.slice(1);
+  let newArr = [];
+  for (let i = 1; i < arr.length; i++) {
+    newArr.push(arr[i]);
+  }
+  return newArr;
 };
 
 
