48774d6560628ee9749a430aca01e057c6a7d987
dganai <dinaganai@gmail.com>
2022-01-17 18:09:03 +0000

    refactor and modularize

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,25 +1,11 @@
-// implenting eqArrays function
-
-const eqArrays = function(array1, array2) {
-  if (array1.length !== array2.length) {
-    return false;
-  }
-  
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-  
-
 // assertArraysEqual will also take in two arrays
 // console.log true or false message to console
 // implement eqArrays function with ternary operator -> condition ? message if true : message if false
-
+const eqArrays = require('./eqArrays');
 const assertArraysEqual = function(array1, array2) {
   eqArrays(array1, array2) ? console.log(true) : console.log(false);
 };
 
-console.log(assertArraysEqual([1, 2, 3], [1, 2, 3]));
+
+
+module.exports = assertArraysEqual;
---
+++ test/assertArraysEqualTest.js
@@ -0,0 +1,5 @@
+const assertArraysEqual = require("../assertArraysEqual");
+
+assertArraysEqual([1, 2, 3], [1, 2, 3]); //true
+assertArraysEqual(["d", "i", "n", "a"], ["d", "i", "n",]); //false
+
